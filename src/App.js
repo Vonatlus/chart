ab1e0a72bdddd61ef162843417214f1b91f95ffb
Alex <asultan4ik@gmail.com>
2022-05-05 23:13:10 +0300

    Height was fixed

--- src/App.js
+++ src/App.js
@@ -19,16 +19,22 @@ function App() {
     switch (type) {
       case 'Bar':
         return Bar;
+
       case 'Line':
         return Line;
+
       case 'Radar':
         return Radar;
+
       case 'Doughnut':
         return Doughnut;
+
       case 'Pie':
         return Pie;
+
       case 'PolarArea':
         return PolarArea;
+
       default:
         return Bar;
     }
@@ -68,31 +74,38 @@ function App() {
         />
       </div>
 
-      <Chart
-        data={{
-          labels,
-          datasets: [{
-            label: 'Date',
-            data: values,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }]
-        }} />
+      <div>
+        <Chart
+          height={400}
+          data={{
+            labels,
+            datasets: [{
+              label: 'Date',
+              data: values,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1,
+            }]
+          }}
+          options={{
+            maintainAspectRatio: false,
+          }}
+        />
+      </div>
 
       <div className="chartType">
         <label>
